ac31f2cede7df2b7000bf906adf9b98519afbb13
Dayrons <dayronstovar@gmail.com>
2023-11-14 08:15:42 -0400

    comienzo de proyecto

---
+++ src/render/App.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import { HashRouter, Route, Routes } from "react-router-dom"
+import './App.scss'
+import { Home } from './pages/Home'
+
+
+
+
+function App() {
+   
+
+    return (
+
+        <HashRouter>
+
+        <Routes>
+          <Route path="/" element={<Home/>} />
+      
+        </Routes>
+
+      </HashRouter>
+
+    )
+}
+
+export default App
