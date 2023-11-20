6c139d2e1d1032210c4882df458111ece8d23aad
Dayrons <dayronstovar@gmail.com>
2023-11-20 10:47:50 -0400

    funcionalidad drag and drop

--- src/render/components/ListPassword.js
+++ src/render/components/ListPassword.js
@@ -1,16 +1,43 @@
 import React from 'react'
 import { Password } from './Password'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import {DndContext, closestCenter} from '@dnd-kit/core'
+import {SortableContext, arrayMove, verticalListSortingStrategy} from '@dnd-kit/sortable'
+import { setPasswords } from '../context/slice/AppSlice';
 export  function ListPassword() {
+  const dispatch = useDispatch()
   const state = useSelector(state => state.app)
+  
+  const handleDragEnd = (e)=>{
+    const {active, over}= e
+    const passwords = state.passwords
+    const oldIndex =  passwords.findIndex((password)=>active.id === password.id)
+    const newIndex =  passwords.findIndex((password)=>over.id === password.id)
+    const newArray = arrayMove(passwords, oldIndex, newIndex)
+    dispatch(setPasswords(newArray))
+  }
+
   return (
     <div style={{
         width:"80%",
         margin:"auto",
         marginTop:"20px"
     }}>
+      <DndContext
+        collisionDetection={closestCenter}
+        onDragEnd={handleDragEnd}
+      >
 
-        {state.passwords.map(password=><Password password={password}/>)}
+        <SortableContext
+        items={state.passwords}
+        strategy={verticalListSortingStrategy}
+        >
+          {state.passwords.map(password=><Password password={password}/>)}
+
+        </SortableContext>
+
+      </DndContext>
+
     </div>
   )
 }
--- src/render/components/Password.js
+++ src/render/components/Password.js
@@ -3,32 +3,72 @@ import IconButton from '@mui/material/IconButton';
 import { IoCopy } from "react-icons/io5";
 import { CopyToClipboard } from 'react-copy-to-clipboard';
 import toast, { Toaster } from 'react-hot-toast';
+import { useSortable } from '@dnd-kit/sortable'
+import { CSS } from '@dnd-kit/utilities'
+import { RiDraggable } from "react-icons/ri";
 export function Password({ password }) {
+
+    const {
+        attributes,
+        listeners,
+        setNodeRef,
+        transform,
+        transition
+    } = useSortable({
+        id: password.id
+    })
+
     return (
         <>
-          <Toaster />
+            <Toaster />
 
-          <div style={{
-            width: "100%",
-            height: "25px",
-            padding: "10px",
-            background: "rgba(43, 46, 61,0.5)",
-            margin: "20px 0",
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center"
-        }}>
-            <h3>{password.titulo}</h3>
+            <div style={{
+                width: "100%",
+                height: "25px",
+                padding: "10px",
+                background: "rgba(43, 46, 61,0.5)",
+                margin: "20px 0",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                transform: CSS.Transform.toString(transform),
+                transition,
+            }}
 
-            <CopyToClipboard text={password.password}
-                onCopy={() => toast('Contraseña copiada')}>
-                <IconButton size='small' >
-                    <IoCopy color='#2CDA9D' />
-                </IconButton>
-            </CopyToClipboard>
-        </div>
+
+
+            >
+                <div style={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}>
+
+                    <div
+                        ref={setNodeRef}
+                        {...attributes}
+                        {...listeners}
+                        style={{
+                            padding: "5px 10px",
+                            cursor:"grab"
+                        }}
+                    >
+                        <RiDraggable />
+
+                    </div>
+                    <h3>{password.titulo}</h3>
+                </div>
+
+
+                <CopyToClipboard text={password.password}
+                >
+                    <IconButton size='small' onClick={(e) => {toast('Contraseña copiada')}}>
+                        <IoCopy color='#2CDA9D' />
+                    </IconButton>
+                </CopyToClipboard>
+            </div>
 
         </>
-        
+
     )
 }
