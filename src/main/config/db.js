ba1df3e63260184eef642b7a5596d8ce36615145
Dayrons <dayronstovar@gmail.com>
2025-06-12 16:35:30 -0400

    fix

--- src/main/config/db.js
+++ src/main/config/db.js
@@ -4,7 +4,7 @@ const { Sequelize } = require('sequelize');
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     // storage: path.join(__dirname, '../','data', 'data.db'),
-    storage: '/home/dayrons/Documentos/Proyectos/lockspace_desktop/src/main/database.db',
+    storage: '/home/user/Documentos/lockspace_desktop/src/main/database.db',
     // logging: false,
     // define: {
     //     freezeTableName: true
