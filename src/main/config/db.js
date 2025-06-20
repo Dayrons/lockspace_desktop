8af8ac227afda73ddfdfe86c7f83b95b4e6aa1f5
Dayrons <dayronstovar@gmail.com>
2025-06-19 23:26:55 -0400

    fix/env_config

--- src/main/config/db.js
+++ src/main/config/db.js
@@ -1,10 +1,10 @@
 const { Sequelize } = require('sequelize');
-
+require('dotenv').config();
 
 const sequelize = new Sequelize({
     dialect: 'sqlite',
     // storage: path.join(__dirname, '../','data', 'data.db'),
-    storage: '/home/user/Documentos/lockspace_desktop/src/main/database.db',
+    storage: process.env.DB_STORAGE,
     // logging: false,
     // define: {
     //     freezeTableName: true
